aa9660283dbff46345ae25eb1875c597252485ff
Błażej <wargun364@gmail.com>
2025-09-13 23:51:16 +0200

    vxfvd

--- src/hooks/useProject/selectors.ts
+++ src/hooks/useProject/selectors.ts
@@ -1,9 +1,9 @@
 import { RootState } from "@/store";
-import { ProjectProp } from "@/types/common";
+import { Project } from "@/types/common";
 
 export const getProjects = (state: RootState) => state.projects;
 export const getProjectById = (payload:string) => (state: RootState) => {
-    const project = state.projects.projectList.find((project: ProjectProp) => project.project_id === payload)
+    const project = state.projects.projectList.find((project: Project) => project.project_id === payload)
     if(project){
         return project
     } else{
